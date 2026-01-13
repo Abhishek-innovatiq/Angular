69b2eea5e6692755369452f6d3149e7dc5fea75f
Abhishekn sen <abhishek@innovatiqconsulting.com>
2026-01-13 18:54:21 +0530

    Form-Based Application

---
+++ forms/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup }
+];
